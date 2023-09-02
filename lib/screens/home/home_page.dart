3204143551a07ff543b88c46ea4d230ccf49230a
Thatchai Khaosamang <athid1997@hotmail.com>
2023-09-03 00:31:28 +0700

    added channel name

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 
 // TODO: ใส่รหัสนักศึกษาที่ค่าสตริงนี้
-const studentId = '630710000';
+const studentId = '630710656';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
